96f12ea9b41aaab5eb320a4a4516a65f8a60bd8b
Jalen Peeler <jalenpeeler@yahoo.com>
2021-06-13 10:40:11 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
+//importing components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div>
       <header>
         <h1>Jalen's Todo List</h1>
       </header>
+      <Form />
+      <TodoList />
     </div>
   );
 }
